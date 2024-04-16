3c49e9e25e2a67bfa3f9379018fb0fcff7ff6ebc
Lianto2226250063 <lianto_2226250063@mhs.mdp.ac.id>
2024-04-16 09:30:37 +0700

    first commit

---
+++ lib/services/notes_service.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class NoteService {
+  final CollectionReference _database =
+      FirebaseFirestore.instance.collection('note_list');
+
+  Stream<Map<String, String>> getNOteList() {
+    return _database.snapshots().map((QuerySnapshot) {
+      final Map<String, String> items = {};
+      QuerySnapshot.docs.map((docSnapshot) {
+        final data = docSnapshot.data() as Map<String, dynamic>;
+        if (data.containsKey('title')) {
+          Map<dynamic, dynamic> values = data as Map<dynamic, dynamic>;
+          values.forEach((key, value) {
+            items[key] = value['title'] as String;
+          });
+        }
+      });
+      return items;
+    });
+  }
+}
